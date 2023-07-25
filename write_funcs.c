d773f8b3603f16aaaafe56cc3c54b28278182bf8
Innocent Manda <106418148+Chipatenii@users.noreply.github.com>
2023-07-25 13:17:57 +0200

    Update write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -3,6 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
+ * By Alex and Innocent
  * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
